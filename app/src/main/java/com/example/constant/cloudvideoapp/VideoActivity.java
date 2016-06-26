b1bef08990693a470a46506508f0bfe136031546
riteshhub <patel.ritesh>
2016-06-26 14:47:07 +0530

    Modified launcher icon and video URL

--- app/src/main/java/com/example/constant/cloudvideoapp/VideoActivity.java
+++ app/src/main/java/com/example/constant/cloudvideoapp/VideoActivity.java
@@ -3,16 +3,20 @@ package com.example.constant.cloudvideoapp;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.VideoView;
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity implements View.OnTouchListener {
 
 
     ProgressBar spinner;
     VideoView videoView;
+    //Uri uri;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,12 @@ public class VideoActivity extends AppCompatActivity {
         spinner = (ProgressBar)findViewById(R.id.my_spinner);
 
         videoView = (VideoView) findViewById(R.id.video_view);
-        //uri.parse("https://googledrive.com/host/0By04j2zmqXZVd05CeVpSNXAyZWc");
         videoView.setVideoPath("https://s3-ap-southeast-1.amazonaws.com/singapore.video.app/video540.mp4");
-        //videoView.setVideoURI(uri);
         videoView.setMediaController(new MediaController(this));
         videoView.requestFocus();
-        videoView.start();
+
+        spinner.setVisibility(View.GONE);
+
         videoView.setOnInfoListener(new MediaPlayer.OnInfoListener()
         {
             @Override
@@ -50,5 +54,21 @@ public class VideoActivity extends AppCompatActivity {
                 }
             }
         });
+
+        videoView.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if(event.getAction()==MotionEvent.ACTION_DOWN)
+        {
+            videoView.start();
+        }
+        else if(event.getAction()==MotionEvent.ACTION_UP)
+        {
+            videoView.stopPlayback();
+        }
+        return true;
     }
 }
